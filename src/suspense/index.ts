1de8a2c5b7249c193ab3432aee0d10e70a780d0d
teawithsand <teawithsand@gmail.com>
2023-07-18 17:50:41 +0000

    Added suspense tools Updated docs

---
+++ src/suspense/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Magical utils which expose `usePromiseSuspense` hook that triggers suspense.
+ * 
+ * It's quite easy to misuse them. Instead react-query https://tanstack.com/query/v3/ is preferred.
+ */
+
+
+export * from "./hook"
+export * from "./wrap"
+export * from "./storage"
